8d0a36a540603b70861baa424ca70e8c53f6b1bc
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-09-15 08:02:45 -0400

    revise flags

--- schemascii/grid.py
+++ schemascii/grid.py
@@ -1,49 +1,28 @@
 class Grid:
     def __init__(self, data: str):
-        self.raw = data
-        lines = data.split('\n')
-        maxlen = max(len(line) for line in lines)
-        self.lines = [list(line.ljust(maxlen, ' ')) for line in lines]
-        self.marked = [[False for x in range(maxlen)] for y in range(len(lines))]
+        self.raw: str = data
+        lines: list[str] = data.split('\n')
+        maxlen: int = max(len(line) for line in lines)
+        self.lines: list[list[str]] = [list(line.ljust(maxlen, ' ')) for line in lines]
+        self.flags: list[list[int]] = [[0 for x in range(maxlen)] for y in range(len(lines))]
 
     def get(self, x: int, y: int) -> str:
-        if self.ismarked(x, y):
+        if self.getflag(x, y) > 0:
             return ' '
         return self.lines[y][x]
     
     def set(self, x: int, y: int, value: str):
         self.lines[y][x] = value
 
-    def ismarked(self, x: int, y: int) -> bool:
-        return self.marked[y][x]
+    def getflag(self, x: int, y: int) -> int:
+        return self.flags[y][x]
     
-    def mark(self, x: int, y: int):
-        self.marked[y][x] = True
+    def setflag(self, x: int, y: int, flag: int = 1):
+        self.flags[y][x] = flag
     
-    def unmark(self, x: int, y: int):
-        self.marked[y][x] = False
-    
-    def __getitem__(self, coords: tuple[int]) -> str:
-        if isinstance(coords, tuple) and len(coords) == 2:
-            return self.get(coords[0], coords[1])
-        else:
-            raise ValueError('Bad coordinates value: %s' % coords)
-    
-    def __setitem__(self, coords: tuple[int], value: bool | str):
-        if isinstance(coords, tuple) and len(coords) == 2:
-            if isinstance(value, bool):
-                if value:
-                    self.mark(coords[0], coords[1])
-                else:
-                    self.unmark(coords[0], coords[1])
-            elif isinstance(value, str) and len(value) == 1:
-                self.set(coords[0], coords[1], value)
-            else:
-                raise ValueError('Bad item value: %s' % value)
-        else:
-            raise ValueError('Bad coordinates value: %s' % coords)
-        return self
+    def clrflag(self, x: int, y: int):
+        self.setflag(x, y, 0)
     
     def __repr__(self):
-        return '\n'.join(''.join(l) for l in self.lines)
+        return "Grid(\n'''\n" + '\n'.join(''.join(l) for l in self.lines) + "'''\n)"
     __str__ = __repr__
