805c3a5624eb0eaba545731a24bec4be16da20bb
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-09-15 08:15:15 -0400

    add data, lines property

--- schemascii/grid.py
+++ schemascii/grid.py
@@ -3,16 +3,20 @@ class Grid:
         self.raw: str = data
         lines: list[str] = data.split('\n')
         maxlen: int = max(len(line) for line in lines)
-        self.lines: list[list[str]] = [list(line.ljust(maxlen, ' ')) for line in lines]
+        self.data: list[list[str]] = [list(line.ljust(maxlen, ' ')) for line in lines]
         self.flags: list[list[int]] = [[0 for x in range(maxlen)] for y in range(len(lines))]
 
     def get(self, x: int, y: int) -> str:
         if self.getflag(x, y) > 0:
             return ' '
-        return self.lines[y][x]
+        return self.data[y][x]
+    
+    @property
+    def lines(self):
+        return [''.join(x) for x in self.data]
     
     def set(self, x: int, y: int, value: str):
-        self.lines[y][x] = value
+        self.data[y][x] = value
 
     def getflag(self, x: int, y: int) -> int:
         return self.flags[y][x]
@@ -24,5 +28,5 @@ class Grid:
         self.setflag(x, y, 0)
     
     def __repr__(self):
-        return "Grid(\n'''\n" + '\n'.join(''.join(l) for l in self.lines) + "'''\n)"
+        return "Grid(\n'''\n" + '\n'.join(self.lines) + "'''\n)"
     __str__ = __repr__
