227cdc01a1eee89af89a2afff18b8b08497ea8de
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-11-02 15:32:17 -0400

    add commented out manhattan router

--- schemascii/utils.py
+++ schemascii/utils.py
@@ -15,16 +15,41 @@ class Side(IntEnum):
 class Wire:
     def __init__(self, points: list[complex]):
         self.points = points
-    def topath(self) -> str:
-        out = ''
+    def render(self) -> str:
+        # prev = self.points[0]
+        # # copied from https://github.com/KenKundert/svg_schematic/blob/0abb5dc/svg_schematic.py#L284-L302
+        # new_points = [prev]
+        # for p in  self.points[1:]:
+        #     if p != prev: # TODO: remove colinear
+        #         candidates = []
+        #         candidates.append([complex(prev.real, p.imag)])
+        #         candidates.append([complex(p.real, prev.imag)])
+        #         ymid = (p.imag + prev.imag)/2
+        #         candidates.append([complex(prev.real, ymid), complex(p.real, ymid)])
+        #         xmid = (p.real + prev.real)/2
+        #         candidates.append([complex(xmid, prev.imag), complex(xmid, p.imag)])
+        #         best = None
+        #         bestscore = float('inf')
+        #         for c in candidates:
+        #             if (s := sharpness_score(new_points + c)) < bestscore:
+        #                 bestscore = s
+        #                 best = c
+        #         new_points.extend(c)
+        #     new_points.append(p)
+        #     prev = p
+        return (
+            '<polyline class="wire" points="'
+            + ' '.join(f'{p.real},{p.imag}' for p in self.points)
+            + '"></polyline>'
+        )
 
 def colinear(points: list[complex]) -> bool:
     return len(set(phase(p-points[0]) for p in points[1:])) == 1
 
 def sharpness_score(points: list[complex]) -> float:
     score = 0
-    prevPoint = points[1]
-    prevPhase = phase(points[1] - points[0])
+    prevPoint = points.imag
+    prevPhase = phase(points.imag - points[0])
     for p in points[2:]:
         ph = phase(p - prevPoint)
         score += abs(prevPhase - ph)
