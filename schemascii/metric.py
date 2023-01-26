0b37d69d191634fcb3b29ce1d5bdfff6e039f727
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-01-26 13:24:46 +0000

    fix metric number handling

--- schemascii/metric.py
+++ schemascii/metric.py
@@ -29,7 +29,7 @@ def prefix_to_exponent(prefix: int) -> str:
     return (i - 4) * 3
 
 
-def normalize_metric(num: str) -> str:
+def format_metric_unit(num: str, unit: str = '') -> str:
     num = num.strip()
     match = METRIC_NUMBER.match(num)
     if not match or not match.group(1):
@@ -47,15 +47,20 @@ def normalize_metric(num: str) -> str:
         exp += 3
     exp += prefix_to_exponent(prefix)
     digits_str = ''.join(map(str, digits))
-    if digits_str.endswith('00') and exp < 0:
-        digits_str = digits_str[:-3] + '.' + digits_str[-3]
-        exp += 3
-    return digits_str + exponent_to_prefix(exp)
+    if exp < 0:
+        while digits_str.endswith('00') and exp < 0:
+            digits_str = digits_str[:-3] + '.' + digits_str[-3]
+            exp += 3
+    else:
+        while digits_str.endswith('00'):
+            digits_str = digits_str[:-3] + '.' + digits_str[-3]
+            exp += 3
+    return digits_str + " " + exponent_to_prefix(exp) + unit
 
 
 if __name__ == '__main__':
-    print(normalize_metric("1500m"))
-    print(normalize_metric("0.05"))
-    print(normalize_metric("1234"))
-    print(normalize_metric("0.47u"))
-    print(normalize_metric("Gain"))
+    print(format_metric_unit("15", "A"))
+    print(format_metric_unit("0.00005", "F"))
+    print(format_metric_unit("1234", "&ohm;"))
+    print(format_metric_unit("0.47u", "F"))
+    print(format_metric_unit("Gain", "&ohm;"))
