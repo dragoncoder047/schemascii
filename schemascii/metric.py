0dfe1cdca1741fb8ff38a8ac730a5ba1650801f5
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-01-30 13:55:41 +0000

    finally fix the metric function

--- schemascii/metric.py
+++ schemascii/metric.py
@@ -2,6 +2,7 @@ import re
 from decimal import Decimal
 
 METRIC_NUMBER = re.compile(r"^(\d*\.?\d+)([pnumKkMG]?)$")  # cSpell:ignore pnum
+ENG_NUMBER = re.compile(r"^(\d*\.?\d+)[Ee]?([+-]?\d*)$")  # cSpell:ignore pnum
 
 
 def exponent_to_prefix(exponent: int) -> str | None:
@@ -37,37 +38,25 @@ def format_metric_unit(num: str, unit: str = '', six: bool = False) -> str:
         return num
     digits_str, prefix = match.group(1), match.group(2)
     digits_decimal = Decimal(digits_str)
-    _, digits, exp = digits_decimal.as_tuple()
-    sig_figs = len(digits)
-    three_six = 6 if six else 3
-    two_five = 5 if six else 2
-    while digits[-three_six:] == (0,) * three_six:
-        digits = digits[:-three_six]
-        exp += three_six
-    digits += (0,) * (exp % 3)
-    exp -= exp % 3
-    while digits[-three_six:] == (0,) * three_six:
-        digits = digits[:-three_six]
-        exp += three_six
-    if six and exp % 6 != 0 and exp < 0:
-        digits += (0, 0, 0)
-        exp -= 3
-    exp += prefix_to_exponent(prefix)
-    digits_str = ''.join(map(str, digits))
-    if digits_str.endswith('0' * two_five) and exp < 0:
-        digits_str = digits_str[:-three_six] + '.' + digits_str[-three_six:]
-        exp += three_six
-        sig_figs += 1
-        if len(digits_str) > sig_figs:
-            digits_str = digits_str.removesuffix('0' * (len(digits_str)
-                                                        - sig_figs))
-    out = digits_str + " " + exponent_to_prefix(exp) + unit
-    return out.replace(" u", " &micro;")
+    digits_decimal *= Decimal('10') ** Decimal(prefix_to_exponent(prefix))
+    res = ENG_NUMBER.match(digits_decimal.to_eng_string())
+    if not res:
+        print('foobar!')
+        return None
+    digits, exp = res.group(1), int(res.group(2) or '0')
+    assert exp % 3 == 0, 'failed to make engineering notation'
+    if six and exp % 6:
+        digits = Decimal(digits)
+        nd_1, nd_2, = str(digits * 1000), str(digits / 1000)
+        exp_1, exp_2 = exp - 3, exp + 3
+        digits = nd_1 if len(nd_1) < len(nd_2) else nd_2
+        exp = exp_1 if len(nd_1) < len(nd_2) else exp_2
+    return digits + " " + exponent_to_prefix(exp) + unit
 
 
 if __name__ == '__main__':
     print(format_metric_unit("2.5", "V"))
-    print(format_metric_unit("0.05", "F", True))
+    print(format_metric_unit("50n", "F", True))
     print(format_metric_unit("1234", "&ohm;"))
     print(format_metric_unit("0.47u", "F", True))
     print(format_metric_unit("Gain", "&ohm;"))
