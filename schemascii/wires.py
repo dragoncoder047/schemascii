532669b1a75ea39eaca38614f8744ba2375f10e2
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-01-30 17:14:01 +0000

    put these changes in commit history

--- schemascii/wires.py
+++ schemascii/wires.py
@@ -9,30 +9,16 @@ DIRECTIONS = [1, -1, 1j, -1j]
 
 
 def is_end(grid: Grid, point: complex) -> bool:
-    """Returns if the point contains a "loose end" wire."""
+    """Returns true if the point is a loose end."""
     char = grid.get(point)
     if char not in "|()-*":
         return False
-    if char in "|()":
-        diffs = [1j, -1j]
-        conn = "|()*"
-        cross = "-"
-    elif char == "-":
-        diffs = [1, -1]
-        conn = "-*"
-        cross = "|()"
-    elif char == '*':
-        diffs = [1, -1, 1j, -1j]
-        conn = "|()-*"
-        cross = ""
-    count_conn = 0
-    for d in diffs:
+    count = 0
+    for d in DIRECTIONS:
         c = grid.get(point + d)
-        while (c := grid.get(point + d)) in cross:
-            point += d
-        if c in conn:
-            count_conn += 1
-    return count_conn < 2
+        if c in "|()-*":
+            count += 1
+    return count < 2
 
 
 def next_in_dir(
@@ -47,6 +33,7 @@ def next_in_dir(
         case '|' | '(' | ')':
             # extend up or down
             if dydx in (1j, -1j):
+                print("extending vertically from", point, dydx)
                 while grid.get(point) in '-|()':
                     point += dydx
                 if grid.get(point) != '*':
@@ -56,6 +43,7 @@ def next_in_dir(
         case '-':
             # extend sideways
             if dydx in (1, -1):
+                print("extending sideways from", point, dydx)
                 while grid.get(point) in '-|()':
                     point += dydx
                 if grid.get(point) != '*':
@@ -64,42 +52,26 @@ def next_in_dir(
                 return None  # The horizontal wires do not connect vertically
         case '*':
             # extend any direction
+            print("asterisk at", point, dydx)
             if grid.get(point + dydx) in '-|()':
-                return next_in_dir(grid, point + dydx, dydx)
+                res = next_in_dir(grid, point + dydx, dydx)
+                if res is not None:
+                    point = res[0]
+                else:
+                    return None
             if grid.get(point + dydx) == '*':
                 point += dydx
             else:
                 return None
         case _:
             return None
+    print('result', old_point, point)
     if point == old_point:
         return None  # suppress length=0 wires
     return point, old_point
 
 
-def search_wire(grid: Grid, point: complex) -> list[tuple[complex, complex]]:
-    """Flood-fills the grid starting at the point, and returns
-    the list of all the straight pieces of wire encountered."""
-    seen = [point]
-    out = []
-    frontier = [point]
-    # find all the points
-    while frontier:
-        here = frontier.pop()
-        for d in DIRECTIONS:
-            line = next_in_dir(grid, here, d)
-            print(here, line)
-            if line is None:
-                continue
-            p = line[0]
-            if p not in seen and p != here:
-                frontier.append(p)
-                seen.append(p)
-                out.append(line)
-    return out
-
-
-def first_end(grid: Grid) -> complex | None:
+def first_end_or_corner(grid: Grid) -> complex | None:
     """Returns the first "loose end" of a wire in the grid."""
     indexes = [complex(x, y) for y in range(grid.height)
                for x in range(grid.width) if is_end(grid, complex(x, y))]
@@ -108,57 +80,65 @@ def first_end(grid: Grid) -> complex | None:
     return None
 
 
-def next_wire(grid: Grid, **options) -> str | None:
+def next_line(grid: Grid) -> tuple[complex, complex] | None:
     """Returns a SVG string of the next line in the grid,
     or None if there are no more. The line is masked off."""
-    scale = options.get("scale", 1)
-    stroke_width = options.get("stroke_width", 1)
-    color = options.get("stroke", "black")
+
     # Find the first loose end or return None
-    end = first_end(grid)
-    if end is None:
+    start = first_end_or_corner(grid)
+    if start is None:
         return None
-    line_pieces = search_wire(grid, end)
-    if not line_pieces:
-        # got a length 0 wire
-        grid.setmask(end)
-        return ""
-    # Blank out the used wire
-    for p1, p2 in line_pieces:
-        # Crazy math!!
-        way = int(phase(p1 - p2) / pi % 1.0 * 2)
-        for px in iterate_line(p1, p2):
-            old = grid.get(px)
-            # way: 0: Horizontal, 1: Vertical
-            if old not in ["|()", "-"][way]:
-                grid.setmask(px)  # Don't mask out wire crosses
-    return XML.g(
-        *(
-            XML.line(
-                x1=p1.real * scale,
-                y1=p1.imag * scale,
-                x2=extend(p1, p2).real * scale,
-                y2=extend(p1, p2).imag * scale,
-                stroke__width=stroke_width,
-                stroke=color,
-            )
-            for p1, p2 in line_pieces
-        ),
-        class_="wire")
+    for diff in DIRECTIONS:
+        line = next_in_dir(grid, start, diff)
+        if line is not None and abs(line[0] - line[1]) > 0:
+            start, end = line
+            break
+    else:
+        return None
+    # Crazy math!!
+    way = int(phase(start - end) / pi % 1.0 * 2)
+    for px in iterate_line(start, end):
+        old = grid.get(px)
+        if old == "*":
+            continue  # Don't mask out wire crosses just yet
+        # way: 0: Horizontal, 1: Vertical
+        if old not in ["|()", "-"][way]:
+            grid.setmask(px)
+    for px in (start, end):
+        old = grid.get(px)
+        if old == "*" and not is_end(grid, px):
+            grid.setmask(px)
+    return start, end
 
 
 def get_wires(grid: Grid, **options) -> str:
     "Finds all the wires and masks them out, returns an SVG string."
-    out = ""
-    w = next_wire(grid, **options)
+    scale = options.get("scale", 1)
+    stroke_width = options.get("stroke_width", 1)
+    color = options.get("stroke", "black")
+    all_lines = []
+    w = next_line(grid)
     while w is not None:
-        out += w
-        w = next_wire(grid, **options)
-    return out
+        all_lines.append(w)
+        print(all_lines, grid)
+        w = next_line(grid)
+    # return XML.g(
+    #     *(
+    #         XML.line(
+    #             x1=p1.real * scale,
+    #             y1=p1.imag * scale,
+    #             x2=extend(p1, p2).real * scale,
+    #             y2=extend(p1, p2).imag * scale,
+    #             stroke__width=stroke_width,
+    #             stroke=color,
+    #         )
+    #         for p1, p2 in line_pieces
+    #     ),
+    #     class_="wire")
 
 
 if __name__ == '__main__':
     with open('../test_data/test_resistors.txt') as f:
         x = Grid('test_resistors.txt', f.read())
-        print(get_wires(x, 1))
+        print(get_wires(x))
         print(x)
