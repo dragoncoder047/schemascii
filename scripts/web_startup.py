b323c9a295ad9a3d4d8e071d928b2e89ef7ec598
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-08-11 19:29:51 +0000

    kludges on event handlers

--- scripts/web_startup.py
+++ scripts/web_startup.py
@@ -1,43 +1,17 @@
-import asyncio
 import functools
 import sys
 import re
 import js
 import json
 import warnings
-from pyodide.http import pyfetch as fetch, open_url as get
-from pyodide.ffi import create_proxy as event_handler
+from pyodide.http import open_url as get
 import micropip
 
 
-def debounced(fun):
-    timeout = None
-
-    @functools.wraps(fun)
-    def inner():
-        nonlocal timeout
-        if timeout:
-            js.clearTimeout(timeout)
-        timeout = js.setTimeout(fun, 100)
-
-    return inner
-
-
-def syncify(fun):
-    @functools.wraps(fun)
-    def inner(e):
-        return asyncio.ensure_future(fun(e))
-    return inner
-
-
-@event_handler
-@debounced
 def sync_css():
     style_elem.innerHTML = css_box.value
 
 
-@event_handler
-@debounced
 def render_catch_warnings(*args, **kwargs):
     import schemascii
     console.textContent = ""
@@ -49,20 +23,22 @@ def render_catch_warnings(*args, **kwargs):
     return out
 
 
-@event_handler
-@syncify
-async def switch_version():
+def switch_version():
     if "schemascii" in sys.modules:
         del sys.modules["schemascii"]  # Invalidate cache
     version = ver_switcher.value
-    await micropip.install(versions_to_wheel_map[version])
+    js.eval(
+        f'''(async () => await pyodide.globals.get("micropip").install({versions_to_wheel_map[version]!r}))();''')
 
-@event_handler
+
 def download_svg():
     a = js.document.createElement("a")
-    a.setAttribute("href", js.URL.createObjectURL(js.Blob.new([output.innerHTML], {"type": "application/svg+xml"})))
-    a.setAttribute("download", f"schemascii_playground_{js.Date.new.toISOString()}_no_css.svg")
+    a.setAttribute("href", js.URL.createObjectURL(js.Blob.new(
+        [output.innerHTML], {"type": "application/svg+xml"})))
+    a.setAttribute(
+        "download", f"schemascii_playground_{js.Date.new.toISOString()}_no_css.svg")
     a.click()
+
 
 output = js.document.getElementById("output")
 css_box = js.document.getElementById("css")
@@ -98,11 +74,3 @@ await micropip.install(versions_to_wheel_map[latest_version])
 css_source = get("schemascii_example.css").read()
 style_elem.textContent = css_source
 css_box.value = css_source
-
-css_box.addEventListener("input", sync_css)
-source.addEventListener("input", render_catch_warnings)
-download_button.addEventListener("click", download_svg)
-
-source.removeAttribute("disabled")
-css_box.removeAttribute("disabled")
-console.textContent = "ready\n"
