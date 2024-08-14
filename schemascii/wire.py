2c169283ababf6b4b22a6fb5d4919df9865e832e
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-08-14 14:19:08 -0400

    modify to include wire configurations on Wire class

--- schemascii/wire.py
+++ schemascii/wire.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
+
 from collections import defaultdict
+from dataclasses import dataclass
 from itertools import combinations
-from typing import Literal
+from typing import ClassVar, Literal
 
 from .grid import Grid
 from .utils import bunch_o_lines
@@ -9,56 +11,63 @@ from .utils import bunch_o_lines
 # This is a map of the direction coming into the cell
 # to the set of directions coming "out" of the cell.
 DirStr = Literal["^", "v", "<", ">"]
-NOWHERE = None
 EVERYWHERE: defaultdict[DirStr, str] = defaultdict(lambda: "<>^v")
 IDENTITY: dict[DirStr, str] = {">": ">", "^": "^", "<": "<", "v": "v"}
-WIRE_DIRECTIONS: defaultdict[str, defaultdict[DirStr, str]] = defaultdict(
-    lambda: NOWHERE, {
-        "-": IDENTITY,
-        "|": IDENTITY,
-        "(": IDENTITY,
-        ")": IDENTITY,
-        "~": IDENTITY,
-        ":": IDENTITY,
-        "*": EVERYWHERE,
-    })
-WIRE_STARTS: defaultdict[str, str] = defaultdict(lambda: NOWHERE, {
-    "-": "<>",
-    "|": "^v",
-    "(": "^v",
-    ")": "^v",
-    "*": "<>^v"
-})
 
 CHAR2DIR: dict[DirStr, complex] = {">": -1, "<": 1, "^": 1j, "v": -1j}
 
 
-class Wire(list[complex]):
+@dataclass
+class Wire:
     """List of grid points along a wire."""
+
+    directions: ClassVar[
+        defaultdict[str, defaultdict[DirStr, str]]] = defaultdict(
+        lambda: None, {
+            "-": IDENTITY,
+            "|": IDENTITY,
+            "(": IDENTITY,
+            ")": IDENTITY,
+            "~": IDENTITY,
+            ":": IDENTITY,
+            "*": EVERYWHERE,
+        })
+    starting_directions: ClassVar[
+        defaultdict[str, str]] = defaultdict(
+        lambda: None, {
+            "-": "<>",
+            "|": "^v",
+            "(": "^v",
+            ")": "^v",
+            "*": "<>^v"
+        })
+
+    # the sole member
+    points: list[complex]
 
     @classmethod
     def get_from_grid(cls, grid: Grid, start: complex) -> Wire:
         seen: set[complex] = set()
-        pts: list[complex] = []
+        points: list[complex] = []
         stack: list[tuple[complex, DirStr]] = [
-            (start, WIRE_STARTS[grid.get(start)])]
+            (start, cls.starting_directions[grid.get(start)])]
         while stack:
             point, directions = stack.pop()
             if point in seen:
                 continue
             seen.add(point)
-            pts.append(point)
+            points.append(point)
             for dir in directions:
                 next_pt = point + CHAR2DIR[dir]
-                if ((next_dirs := WIRE_DIRECTIONS[grid.get(next_pt)])
-                        is not NOWHERE):
+                if ((next_dirs := cls.directions[grid.get(next_pt)])
+                        is not None):
                     stack.append((next_pt, next_dirs[dir]))
-        return cls(pts)
+        return cls(points)
 
     def to_xml_string(self, **options) -> str:
         # create lines for all of the neighbor pairs
         links = []
-        for p1, p2 in combinations(self, 2):
+        for p1, p2 in combinations(self.points, 2):
             if abs(p1 - p2) == 1:
                 links.append((p1, p2))
         return bunch_o_lines(links, **options)
