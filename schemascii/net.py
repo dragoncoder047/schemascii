c37d58c4f91b2837f6c49df6f4f6f49989b6baec
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2024-08-17 18:18:14 -0400

    add net grouping by wire tag

--- schemascii/net.py
+++ schemascii/net.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 
 import schemascii.grid as _grid
 import schemascii.wire as _wire
+import schemascii.wire_tag as _wt
 
 
 @dataclass
@@ -18,13 +19,36 @@ class Net:
     def find_all(cls, grid: _grid.Grid) -> list[Net]:
         seen_points: set[complex] = set()
         all_nets: list[cls] = []
+        all_tags = _wt.WireTag.find_all(grid)
 
         for y, line in enumerate(grid.lines):
             for x, ch in enumerate(line):
                 if _wire.Wire.is_wire_character(ch):
-                    wire = _wire.Wire.get_from_grid(grid, complex(x, y))
+                    wire = _wire.Wire.get_from_grid(
+                        grid, complex(x, y), all_tags)
                     if all(p in seen_points for p in wire.points):
                         continue
-                    all_nets.append(cls([wire]))
+                    # find existing net or make a new one
+                    for net in all_nets:
+                        if any(w.tag is not None
+                               and wire.tag is not None
+                               and w.tag.name == wire.tag.name
+                               for w in net.wires):
+                            net.wires.append(wire)
+                            break
+                    else:
+                        all_nets.append(cls([wire]))
                     seen_points.update(wire.points)
         return all_nets
+
+
+if __name__ == '__main__':
+    g = _grid.Grid("", """
+=wrap1>------C1------=wrap1>
+<wrap2=------C2------<wrap3=
+""")
+    nets = Net.find_all(g)
+    for net in nets:
+        print("---NET---")
+        for wire in net.wires:
+            g.spark(*wire.points)
